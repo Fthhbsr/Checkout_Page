05a130c24a8eadf7f5ba5b4baad84f2efb3b2a60
fatih <fthhbsr@gmail.com>
2022-06-02 21:02:01 +0300

    button clicks finish

--- checkout.js
+++ checkout.js
@@ -4,4 +4,23 @@ const shippingPrice = 15.0;
 window.addEventListener("load", () => {
   localStorage.setItem("taxRate", taxRate);
   localStorage.setItem("shippingPrice", shippingPrice);
+
+  //   sessionStorage.setItem("taxRate", taxRate);
+  //   sessionStorage.setItem("shippingPrice", shippingPrice);
+});
+
+//capturing method
+
+let productsDiv = document.querySelector(".products");
+productsDiv.addEventListener("click", (event) => {
+  if (event.target.className == "minus") {
+    console.log("minus button clicked");
+  } else if (event.target.classList.contains("plus")) {
+    console.log("plus button clicked");
+  } else if (event.target.classList.contains("remove-product")) {
+    console.log("remove button clilcked");
+  } else {
+    console.log("other elements clicked");
+  }
+  // console.log(event.target);
 });
